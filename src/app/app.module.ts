23d8e34614d40b9df00cea8fc84ab30c6f9ef893
jhorjan martinez <jhorjanmartinez13@hotmail.com>
2024-04-23 17:34:37 -0500

    intentando subir repositorio

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    AppRoutingModule,
+    SharedModule,
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+  styles: ``
+})
+export class LayoutPageComponent {
+  public sidebarItems = [
+    {label:'listado', icon:'label', url:'./list'},
+    {label:'añadir', icon:'add', url:'./new-hero'},
+    {label:'Buscar', icon:'search', url:'./search'},
+  ]
+}
